1a319419b52e983078fde07db1efcf274ee7ac99
ahnanne <54733637+ahnanne@users.noreply.github.com>
2024-11-25 23:38:12 +0900

    [feat] register Category model

--- market/item/models.py
+++ market/item/models.py
@@ -3,3 +3,9 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    class Meta:
+        verbose_name_plural = 'Categories'
+        ordering = ('name',)
+
+    def __str__(self):
+        return str(self.name) # name 필드 값으로 표시
