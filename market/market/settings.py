592998b86a9bc2dce0a0439c8c411298059ca8e2
ahnanne <54733637+ahnanne@users.noreply.github.com>
2024-11-27 00:53:05 +0900

    [chore] manage secret key with env

--- market/market/settings.py
+++ market/market/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t3yn!9!v@4g!2nj77jn$q7w)=nd*26^y_(mt5mzjsf$p17)w=j'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
