97b8f393f92cb48a9493ca5aedbe0789b0871da7
ahnanne <54733637+ahnanne@users.noreply.github.com>
2024-12-04 19:46:18 +0900

    [feat] create signup view

--- market/core/urls.py
+++ market/core/urls.py
@@ -6,5 +6,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('contact/', views.contact, name='contact')
+    path('contact/', views.contact, name='contact'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login')
 ]
--- market/core/views.py
+++ market/core/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
 from item.models import Category, Item
+from .forms import SignupForm
 
 def index(request):
+    print(messages.get_messages(request))
     items = Item.objects.filter(is_sold=False) [0:6]
     categories = Category.objects.all()
 
@@ -12,3 +16,22 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, f"Welcome, {user.username}! Your account has been created.")
+            return redirect('/login/')
+        
+    else:
+        form = SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
+    
+def login(request):
+    return render(request, 'core/login.html')
