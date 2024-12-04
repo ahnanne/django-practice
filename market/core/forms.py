a6adfbbdb68f2ad0d6522092697004a9b342e78f
ahnanne <54733637+ahnanne@users.noreply.github.com>
2024-12-04 21:42:58 +0900

    [feat] add login form

--- market/core/forms.py
+++ market/core/forms.py
@@ -1,28 +1,40 @@
 from django import forms
 
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User  # pylint: disable=imported-auth-user
 
-input_style = 'w-full py-4 px-6 rounded-xl'
+INPUT_STYLE = 'w-full py-4 px-6 rounded-xl'
+
 
 class SignupForm(UserCreationForm):
-  class Meta:
-    model = User
-    fields = ('username', 'email', 'password1', 'password2')
-    
-  username = forms.CharField(widget=forms.TextInput(attrs={
-    'placeholder': 'Your username',
-    'class': input_style
-  }))
-  email = forms.CharField(widget=forms.EmailInput(attrs={
-    'placeholder': 'Your email',
-    'class': input_style
-  }))
-  password1 = forms.CharField(widget=forms.PasswordInput(attrs={
-    'placeholder': 'Your password',
-    'class': input_style
-  }))
-  password2 = forms.CharField(widget=forms.PasswordInput(attrs={
-    'placeholder': 'Repeat password',
-    'class': input_style
-  }))
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your username',
+        'class': INPUT_STYLE
+    }))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': 'Your email',
+        'class': INPUT_STYLE
+    }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Your password',
+        'class': INPUT_STYLE
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Repeat password',
+        'class': INPUT_STYLE
+    }))
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your username',
+        'class': INPUT_STYLE
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Your password',
+        'class': INPUT_STYLE
+    }))
--- market/core/views.py
+++ market/core/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 
 from item.models import Category, Item
-from .forms import SignupForm
+from .forms import SignupForm, LoginForm
+
 
 def index(request):
     print(messages.get_messages(request))
-    items = Item.objects.filter(is_sold=False) [0:6]
+    items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
 
     return render(request, 'core/index.html', {
@@ -14,24 +15,32 @@ def index(request):
         'items': items,
     })
 
+
 def contact(request):
     return render(request, 'core/contact.html')
+
 
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
-        
+
         if form.is_valid():
             user = form.save()
-            messages.success(request, f"Welcome, {user.username}! Your account has been created.")
+            messages.success(
+                request, f"Welcome, {user.username}! Your account has been created.")
             return redirect('/login/')
-        
+
     else:
         form = SignupForm()
 
     return render(request, 'core/signup.html', {
         'form': form
     })
-    
+
+
 def login(request):
-    return render(request, 'core/login.html')
+    form = LoginForm()
+
+    return render(request, 'core/login.html', {
+        'form': form
+    })
