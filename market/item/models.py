e8d9186fb11dc501ba4107148f3ce9d9f49f4e53
ahnanne <54733637+ahnanne@users.noreply.github.com>
2024-12-06 02:02:02 +0900

    [style] auto-format code

--- market/item/models.py
+++ market/item/models.py
@@ -1,7 +1,9 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User  # pylint: disable=imported-auth-user
 from django.db import models
 
 # Create your models here.
+
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
@@ -10,21 +12,23 @@ class Category(models.Model):
         ordering = ('name',)
 
     def __str__(self):
-        return str(self.name) # name 필드 값으로 표시
+        return str(self.name)  # name 필드 값으로 표시
 
 
 class Item(models.Model):
-    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, related_name='items', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True) # longer than 255
+    description = models.TextField(blank=True, null=True)  # longer than 255
     price = models.FloatField()
     image = models.ImageField(upload_to='item_images', blank=True, null=True)
     is_sold = models.BooleanField(default=False)
-    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(
+        User, related_name='items', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ('name',)
 
     def __str__(self):
-        return str(self.name) # name 필드 값으로 표시
+        return str(self.name)  # name 필드 값으로 표시
