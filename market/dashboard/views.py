3c2226a5107c855c3dbc5cc620f80be13eaa201e
ahnanne <54733637+ahnanne@users.noreply.github.com>
2024-12-09 23:22:25 +0900

    [feat] complete dashboard

--- market/dashboard/views.py
+++ market/dashboard/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from utils.common import inject_image
+from item.models import Item
 
 # Create your views here.
+
+
+@login_required
+def index(request):
+    items = Item.objects.filter(created_by=request.user)
+
+    return render(request, 'dashboard/index.html', {
+        'items': list(map(inject_image, items))
+    })
